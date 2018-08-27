b2380020ae11446064c81ccb2ec733df821d6a0c
avilesdiana <avilesdiana@outlook.com>
2018-08-27 17:42:40 -0500

    P6

---
+++ ImplementaOperacionesInst.java
@@ -0,0 +1,27 @@
+/* ------------------------------------------------------------------------------------------------------
+Programación Orientada a Objetos
+
+      Uso de métodos
+      Fecha:
+   @Author: Diana Aviles
+
+
+  PROGRAMA 6
+    ---------------------------------------------------------------------------------------------------*/
+
+    public class ImplementaOperacionesInst {
+
+    public static void main(String[] args){
+
+      OperacionesInst ejecutaOperac =new OperacionesInst(4,5);//inicializar constructor
+
+      System.out.println("_________________________________________");
+      System.out.println("Suma,Resta,Multiplicacion,Division");
+      System.out.println("By: Diana Aviles");
+      System.out.println("_________________________________________\n");
+      ejecutaOperac.RealizaSuma();
+      ejecutaOperac.RealizaResta();
+      ejecutaOperac.RealizaMultiplicacion();
+      ejecutaOperac.RealizaDivision();
+    }
+    }
